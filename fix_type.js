a14c2a1810d623f718fa07ffbd0365239d541b6d
Tyrone Penaflor <penaflor.tyrone@gmail.com>
2026-09-14 15:07:50 -0700

    chore: remove ad-hoc scratch scripts

--- fix_type.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/lib/segmentDetection.ts', 'utf-8');
-
-content = content.replace(
-  'lengthMeters: number;\n};',
-  'lengthMeters: number;\n  name?: string;\n  isCustom?: boolean;\n};'
-);
-
-fs.writeFileSync('src/lib/segmentDetection.ts', content);
